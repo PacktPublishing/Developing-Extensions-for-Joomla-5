78d866f427c91d411d4973e3447b0a76653788f6
Carlos Cámara <carlos@hepta.es>
2023-10-20 17:48:48 +0200

    Fixed some deprecations for Joomla 5

--- src/plugins/console/spm/src/CliCommand/SpmTaskDeadlineCommand.php
+++ src/plugins/console/spm/src/CliCommand/SpmTaskDeadlineCommand.php
@@ -8,53 +8,88 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Joomla\Console\Command\AbstractCommand;
 use Joomla\CMS\Language\Text;
+use Symfony\Component\Console\Input\InputOption;
+use Joomla\CMS\Factory;
 
 class SpmTaskDeadlineCommand extends AbstractCommand
 {
     protected static $defaultName = 'spm:task:deadline';
 
+    private $cliInput;
+
     protected function configure(): void
     {
         $this->setDescription(Text::_('PLG_CONSOLE_SPM_TASK_DEADLINE_DESCRIPTION'));
         $this->setHelp(Text::_('PLG_CONSOLE_SPM_TASK_DEADLINE_HELP'));
+
+        $this->addOptions();
     }
 
-}
+    protected function addOptions()
+    {
+        $description = Text::_('PLG_CONSOLE_SPM_TASK_DEADLINE_OPTION_DAYS_DESCRIPTION');
+        $this->addOption('days', 'd', InputOption::VALUE_OPTIONAL, $description, 7);
 
-protected function getDeadlines() : array
-{
-    $deadlines = [];
-
-    $days = 7;
-
-    $db = $this->getDatabase();
-    $query = $db->getQuery(true);
-    $query->select('*')
-        ->from('#__spm_tasks');
-
-    $query->where('deadline BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL ' . $days .' DAY)');
-
-    $query->setQuery($query);
-
-    $deadlines = $db->loadAssocList('id');
-
-    return $deadlines;
-}
-
-protected function doExecute(InputInterface $input, OutputInterface $output):  int
-{
-    $outputStyle = new SymfonyStyle($input, $output);
-    $outputStyle->title(Text::_('PLG_CONSOLE_SPM_TASK_DEADLINE_TITLE'));
-
-    $deadlines = $this->getDeadlines();
-
-    if (empty($deadlines)) {
-        $outputStyle->note(Text::_('PLG_CONSOLE_SPM_TASK_DEADLINE_NO_DEADLINE'));
-    } else {
-        $outputStyle->table([Text::_('PLG_CONSOLE_SPM_TASK_DEADLINE_DEADLINE_HEADER'), 'Project', 'Task'], $deadlines);
+        return;
     }
 
+    protected function getDatabase()
+    {
+        $db = Factory::getDbo();
 
-    return Command::SUCCESS;
-}
+         return $db;
+    }
+
+    protected function getDeadlines($options) : array
+    {
+        $deadlines = [];
+
+        $days = (int)$options['days'];
+
+        if ($days <= 0) {
+            $days = 7;
+        }
+
+        $db = $this->getDatabase();
+        $query = $db->getQuery(true);
+        $query->select('*')
+            ->from('#__spm_tasks');
+
+        $query->where('deadline BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL ' . $days . ' DAY)');
+
+        $query->setQuery($query);
+
+        $deadlines = $db->loadAssocList('id');
+
+        return $deadlines;
+    }
+
+    protected function getOptions() : array
+    {
+        $options = [];
+
+        $options = $this->cliInput->getOptions();
+
+        return $options;
+    }
+
+    protected function doExecute(InputInterface $input, OutputInterface $output):  int
+    {
+        $this->cliInput = $input;
+
+        $outputStyle = new SymfonyStyle($input, $output);
+        $outputStyle->title(Text::_('PLG_CONSOLE_SPM_TASK_DEADLINE_TITLE'));
+
+        $options = $this->getOptions();
+
+        $deadlines = $this->getDeadlines($options);
+
+        if (empty($deadlines)) {
+            $outputStyle->note(Text::_('PLG_CONSOLE_SPM_TASK_DEADLINE_NO_DEADLINE'));
+        } else {
+            $outputStyle->table([Text::_('PLG_CONSOLE_SPM_TASK_DEADLINE_DEADLINE_HEADER'), 'Project', 'Task'], $deadlines);
+        }
+
+        return Command::SUCCESS;
+    }
 }
